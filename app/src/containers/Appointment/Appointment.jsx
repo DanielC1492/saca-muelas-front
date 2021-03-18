eb9f027bff351fbe3242c45deb1b044109ab681a
devian5 <acgarage@outlook.es>
2021-03-18 09:25:34 +0100

    I have created a map to show all the appointments of a client

--- app/src/containers/Appointment/Appointment.jsx
+++ app/src/containers/Appointment/Appointment.jsx
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import { NavbarProfile } from '../../components/Navbar/Navbar'
 import axios from 'axios';
+import './Appointment.css';
 
 const Appointment = () => {
 
-    const [appointment,setAppointment] = useState([])
+    const [appointment,setAppointment] = useState({
+        future: []
+    })
+
     const URL = 'http://localhost:3000/appointment/'
     
     useEffect(() => {
         allData();
-    },[setAppointment]);
+    },[]);
     
     
     const allData = async () => {
@@ -24,25 +28,51 @@ const Appointment = () => {
         }
         const appointmentData = await axios.get(URL, config)
         console.log(appointmentData.data);
-        return appointmentData
+        setAppointment({...appointment, future: appointmentData.data})
     };
 
-    const stateHandler = (event) => {
-        setAppointment({...appointment, [event.target.name]: event.target.type === "number" ? +event.target.value : event.target.value});
-        console.log(setAppointment())
-    }
+    // const clickMe = (argument) => {
+    //     console.log(argument,'ARGUMENT')
+    // }
 
-
-    return (
-        <div>
-            <NavbarProfile/>
-            <button className='loginBtn' onClick={()=> allData()} onChange={stateHandler} >Show</button>
+    console.log(appointment.future,'<===================SET==========>')
+    if(!appointment.future?.result){
+        return (
             <div>
-                {JSON.stringify(appointment, null,2)}
+                <p>LOADING...</p>
             </div>
-
-        </div>
-    )
+        )
+    }else{
+        return (
+            <div>
+                <NavbarProfile/>
+                {/* <button className='loginBtn' onClick={()=> allData()} onChange={stateHandler} >Show</button> */}
+                <div className='mapContainer'>
+                    {appointment.future?.result.map(consultation => {
+                        return (
+                            <div className='dataCollection' key={consultation.id} /*onClick={()=> clickMe(consultation)}*/>
+                                <div className='professional'>
+                                    Professional: {consultation.ProfessionalId}
+                                    {/* moment.format('Do-MMMM-YYYY')} */}
+                                </div>
+                                <div className='id'>
+                                    Appointment: {consultation.id}
+                                </div>
+                                <div className='covid'>
+                                    Covid: {JSON.stringify(consultation.covid)}
+                                </div>
+                                <div className='date'>
+                                    Date: {consultation.date}
+                                </div>
+                            </div>
+                        )
+                    
+                    })}
+                </div>
+    
+            </div>
+        )
+    }
 }
 
 export default Appointment
